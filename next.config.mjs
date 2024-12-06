a8294504d31636776537ba81ae3ca1f277c3b157
Joseph Mwarishi <43416979+josanta@users.noreply.github.com>
2024-12-06 15:23:16 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['westernfinance.org'],
+      },
+};
 
 export default nextConfig;
